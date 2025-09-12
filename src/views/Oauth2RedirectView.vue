f440a5c078cddb1efaff374101c388bc676372c5
amicitia3600 <dyungwoo3600@gmail.com>
2025-09-12 20:52:26 +0900

    [feature]oauth2

--- src/views/Oauth2RedirectView.vue
+++ src/views/Oauth2RedirectView.vue
@@ -12,10 +12,10 @@ const authStore = useAuthStore();
 
 onMounted(async () => {
   try {
-    // ✅ 최초 OAuth2 로그인 → Refresh 쿠키 기반으로 AccessToken 발급
+    //  최초 OAuth2 로그인 → Refresh 쿠키 기반으로 AccessToken 발급
     await authStore.loginWithOAuth2();
 
-    // ✅ 로그인 성공 후 대시보드 이동
+    //  로그인 성공 후 대시보드 이동
     const slug = authStore.companySlug;
     router.push(`/${slug}/dashboard`);
   } catch (err) {
